86b22d764b98173b5699d00271721e90a54a5f74
Emelie Forselius <emelieforselius@outlook.com>
2024-08-31 16:26:27 +0200

    implementerat grundl. struktur och funktionalitet

--- src/App.jsx
+++ src/App.jsx
@@ -5,4 +5,40 @@ import Login from "./components/Login";
 import Chat from "./components/Chat";
 import SideNav from "./components/SideNav";
 
+const App = () => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const storedUser = JSON.parse(localStorage.getItem('user'));
+    if (storedUser) {
+        setUser(storedUser);
+    }
+}, []);
+
+const handleLogin = (userData) => {
+  setUser(userData);
+    localStorage.setItem('user', JSON.stringify(userData));
+};
+
+const handleLogout = () => {
+  setUser(null);
+  localStorage.removeItem('user');
+  localStorage.removeItem('token');
+};
+
+return (
+<Router>
+  <div>
+    {user && <SideNav user={user} onLogout={handleLogout}/>}
+    <Routes>
+      <Route path="/register" element={!user ? <Register/> : <Navigate to="/chat"/>} />
+      <Route path="/login" element={!user ? <Login onLogin={handleLogin}/> : <Navigate to="/chat"/> } />
+      <Route path="/chat" element={user ? <Chat user={user} /> : <Navigate to="/login" />} />
+      <Route path="/" element={<Navigate to={user ? "/chat" : "/login"} />} />
+    </Routes>
+  </div>
+</Router>
+);
+};
+
 export default App;
