afdf7ca41d7356b141edc2cfa0969420cafa100e
Emelie Forselius <emelieforselius@outlook.com>
2024-10-03 12:54:16 +0200

    Uppdaterat App & main för routing & autentisering

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,26 @@
-import React, {useState, useEffect} from "react";
-import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import React, { useContext } from "react";
+import { AuthContext, AuthProvider } from "./components/AuthProvider";
 import Register from "./components/Register";
 import Login from "./components/Login";
 import Chat from "./components/Chat";
-import SideNav from "./components/SideNav";
 import styles from "./components/App.module.css";
+import ProtectedRoute from "./components/ProtectedRoute";
+import { Route, Routes } from "react-router-dom";
+import Layout from "./components/Layout";
 
 const App = () => {
-  const [user, setUser] = useState(null);
+  return (
+    <AuthProvider>
+          <Routes>
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
 
-  useEffect(() => {
-    const storedUser = JSON.parse(localStorage.getItem('user'));
-
-    if (storedUser) {
-        setUser(storedUser);
-    }
-}, []);
-
-const handleLogin = (userData) => {
-
-  setUser(userData);
-    localStorage.setItem('user', JSON.stringify(userData));
-};
-
-const handleLogout = () => {
-  setUser(null);
-  localStorage.removeItem('user');
-  localStorage.removeItem('token');
-  Navigate('/login');
-};
-
-return (
-<Router>
-  <div className={styles.appContainer}>
-    {user && <SideNav user={user} onLogout={handleLogout}/>}
-    <div className={styles.mainContent}>
-    <Routes>
-      <Route path="/register" element={!user ? <Register/> : <Navigate to="/chat"/>} />
-      <Route path="/login" element={!user ? <Login onLogin={handleLogin}/> : <Navigate to="/chat"/> } />
-      <Route path="/chat" element={user ? <Chat user={user} /> : <Navigate to="/login" />} />
-      <Route path="/" element={<Navigate to={user ? "/chat" : "/login"} />} />
-    </Routes>
-    </div>
-  </div>
-</Router>
-);
+            <Route element={<ProtectedRoute />}>
+              <Route path="/chat" element={<Layout><Chat /></Layout>} />
+            </Route>
+          </Routes>
+    </AuthProvider>
+  );
 };
 
 export default App;
