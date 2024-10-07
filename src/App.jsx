940b91e5e50ed7656a2ef45644733e6a4a089ade
Emelie Forselius <emelieforselius@outlook.com>
2024-10-07 14:53:25 +0200

    Fixed missing index.html

--- src/App.jsx
+++ src/App.jsx
@@ -5,25 +5,32 @@ import Login from "./components/Login";
 import Chat from "./components/Chat";
 import styles from "./components/App.module.css";
 import ProtectedRoute from "./components/ProtectedRoute";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 import Layout from "./components/Layout";
 
 const App = () => {
   return (
     <AuthProvider>
-      <div className={styles.appContainer}>  
-      <div className={styles.mainContent}>
+      <div className={styles.appContainer}>
+        <div className={styles.mainContent}>
           <Routes>
-            <Route path="/" element={<Navigate to="/login" />}/>
+            <Route path="/" element={<Navigate to="/login" />} />
             <Route path="/register" element={<Register />} />
             <Route path="/login" element={<Login />} />
 
             <Route element={<ProtectedRoute />}>
-              <Route path="/chat" element={<Layout><Chat /></Layout>} />
+              <Route
+                path="/chat"
+                element={
+                  <Layout>
+                    <Chat />
+                  </Layout>
+                }
+              />
             </Route>
           </Routes>
-          </div>
-          </div>
+        </div>
+      </div>
     </AuthProvider>
   );
 };
